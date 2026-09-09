243fc8ffd736dd93663bb1ed58732c64cfdade14
marcbran <git@marcbrandenburg.net>
2026-09-09 07:41:07 +0200

    feat: plugin with options pattern (#45)

--- kubernetes/plugin.go
+++ kubernetes/plugin.go
@@ -5,9 +5,13 @@ import (
 	"github.com/marcbran/jpoet/pkg/jpoet"
 )
 
-func Plugin() *jpoet.Plugin {
+func Plugin(opts ...jpoet.PluginOption) *jpoet.Plugin {
 	clients := newClientRegistry()
 	wc := newWatchCache(clients)
+	allOpts := append([]jpoet.PluginOption{
+		jpoet.WithWatchSource(wc),
+		jpoet.WithCloser(wc),
+	}, opts...)
 	return jpoet.NewPlugin(
 		"kubernetes",
 		[]jsonnet.NativeFunction{
@@ -15,6 +19,6 @@ func Plugin() *jpoet.Plugin {
 			Get(wc),
 			NeatGet(wc),
 		},
-		jpoet.WithWatchSource(wc),
-	).WithCloser(wc)
+		allOpts...,
+	)
 }
